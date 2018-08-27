186c20bde82753a7e96253cea3deba99b78c8619
nv-gordeev <nv-gordeev@ya.ru>
2018-08-27 21:47:50 +0500

    lazy fetching

--- schema.js
+++ schema.js
@@ -10,11 +10,11 @@ const BookType = new GraphQLObjectType({
     fields: () => ({
         title: {
             type: GraphQLString,
-            resolve: xml => xml.title[0]
+            resolve: xml => xml.GoodreadsResponse.book[0].title[0]
         },
         isbn: {
             type: GraphQLString,
-            resolve: xml => xml.isbn[0].$ && xml.isbn[0].$.nil && 'has no isbn' ||  xml.isbn[0]
+            resolve: xml =>xml.GoodreadsResponse.book[0].isbn[0]
         }
     })
 })
@@ -29,7 +29,14 @@ const AuthorType = new GraphQLObjectType({
         },
         books: {
             type: new GraphQLList(BookType),
-            resolve: xml => xml.GoodreadsResponse.author[0].books[0].book
+            resolve: xml => {
+                const ids = xml.GoodreadsResponse.author[0].books[0].book.map(elem =>elem.id[0]._)
+                return Promise.all(ids.map(id => 
+                    fetch(`https://www.goodreads.com/book/show/${id}.xml?key=1un3AYn63yr4ncdWR77A`)
+                    .then(response => response.text())
+                    .then(parseXML)
+                ))
+            }
         }
     })
 })
