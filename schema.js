dba1531971b7af14d513891975745bdaa88fe743
nv-gordeev <nv-gordeev@ya.ru>
2018-08-28 22:26:49 +0500

    caching with dataloader

--- schema.js
+++ schema.js
@@ -1,7 +1,3 @@
-const fetch = require('node-fetch')
-const util = require('util')
-const parseXML = util.promisify(require('xml2js').parseString)
-
 const {GraphQLSchema, GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLList} = require('graphql')
 
 const BookType = new GraphQLObjectType({
@@ -15,6 +11,14 @@ const BookType = new GraphQLObjectType({
         isbn: {
             type: GraphQLString,
             resolve: xml =>xml.GoodreadsResponse.book[0].isbn[0]
+        },
+        authors: {
+            type: new GraphQLList(AuthorType),
+            resolve: (xml, args, context) => {
+                const authorElements = xml.GoodreadsResponse.book[0].authors[0].author
+                const ids = authorElements.map(el => el.id[0])
+                return context.authorLoader.loadMany(ids)
+            }
         }
     })
 })
@@ -29,13 +33,9 @@ const AuthorType = new GraphQLObjectType({
         },
         books: {
             type: new GraphQLList(BookType),
-            resolve: xml => {
+            resolve: (xml, args, context) => {
                 const ids = xml.GoodreadsResponse.author[0].books[0].book.map(elem =>elem.id[0]._)
-                return Promise.all(ids.map(id => 
-                    fetch(`https://www.goodreads.com/book/show/${id}.xml?key=1un3AYn63yr4ncdWR77A`)
-                    .then(response => response.text())
-                    .then(parseXML)
-                ))
+                return context.bookLoader.loadMany(ids)
             }
         }
     })
@@ -51,9 +51,7 @@ module.exports = new GraphQLSchema({
                 args: {
                     id: {type: GraphQLInt}
                 },
-                resolve: (root, args) => fetch(`https://www.goodreads.com/author/show.xml?key=1un3AYn63yr4ncdWR77A&id=${args.id}`)
-                    .then(response => response.text())
-                    .then(parseXML)
+                resolve: (root, args, context) => context.authorLoader.load(args.id)
             }
         })
         
--- serve.js
+++ serve.js
@@ -1,11 +1,34 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const app = express()
+const DataLoader = require('dataloader')
+const fetch = require('node-fetch')
+const util = require('util')
+const parseXML = util.promisify(require('xml2js').parseString)
 
 const schema = require('./schema.js')
 
+const fetchAuthor = (id) =>
+    fetch(`https://www.goodreads.com/author/show.xml?key=1un3AYn63yr4ncdWR77A&id=${id}`)
+        .then(response => response.text())
+        .then(parseXML)
+
+
+const fetchBooks = id => 
+    fetch(`https://www.goodreads.com/book/show/${id}.xml?key=1un3AYn63yr4ncdWR77A`)
+    .then(response => response.text())
+    .then(parseXML)
+
+const authorLoader = new DataLoader(keys => Promise.all(keys.map(fetchAuthor)))
+const bookLoader = new DataLoader(keys => Promise.all(keys.map(fetchBooks)))
+
+
 app.use('/graphql', graphqlHTTP({
     schema,
+    context: {
+        authorLoader,
+        bookLoader
+    },
     graphiql: true
 }))
 
